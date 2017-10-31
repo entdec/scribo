3d1ed65cd6d060cc0430b3053b7a29a9f1e28c64
Tom de Grunt <tom@degrunt.nl>
2017-10-31 12:03:19 +0100

    Add import capability

--- app/models/scribo/site.rb
+++ app/models/scribo/site.rb
@@ -18,18 +18,49 @@ module Scribo
         meta_info_entry = zip_file.glob('site_*/._site.json').first
         return unless meta_info_entry
 
-        puts "meta_info_entry: #{JSON.parse(meta_info_entry.get_input_stream.read)}"
+        meta_info_site = JSON.parse(meta_info_entry.get_input_stream.read)
+        # TODO: Check version numbers
+        site = Site.where(scribable_type: meta_info_site['scribable_type'], scribable_id: meta_info_site['scribable_id'])
+                 .where(name: meta_info_site['name']).first
+        site ||= Site.create(scribable_type: meta_info_site['scribable_type'], scribable_id: meta_info_site['scribable_id'], name: meta_info_site['name'])
 
         zip_file.glob('**/*').reject { |e| File.basename(e.name)[0..1] == '._' }.each do |entry|
+
+          import_path = entry.name[entry.name.index('/')..-1].chomp('.html')
+
           meta_info_name  = File.dirname(entry.name) + '/._' + File.basename(entry.name) + '.json'
           meta_info_entry = zip_file.find_entry(meta_info_name)
 
           meta_info = JSON.parse(meta_info_entry.get_input_stream.read)
-          puts meta_info
+
+          content = if meta_info['identifier']
+                      site.contents.find_or_create_by(site: site, identifier: meta_info['identifier'])
+                    elsif meta_info['name']
+                      site.contents.find_or_create_by(site: site, name: meta_info['name'])
+                    else
+                      site.contents.find_or_create_by(site: site, path: import_path)
+
+                    end
+
+          content.data         = entry.get_input_stream.read
+          content.kind         = meta_info['kind']
+          content.content_type = meta_info['content_type']
+          content.title        = meta_info['title']
+          content.description  = meta_info['description']
+          content.filter       = meta_info['filter']
+          content.caption      = meta_info['caption']
+          content.breadcrumb   = meta_info['breadcrumb']
+          content.keywords     = meta_info['keywords']
+          content.state        = meta_info['state']
+          content.layout       = site.contents.find_by(identifier: meta_info['layout']) if meta_info['layout']
+          content.properties   = meta_info['properties']
+          content.published_at = meta_info['published_at']
+          content.save
+
         end
       end
-      nil
 
+      nil
     end
 
     def export
@@ -44,11 +75,11 @@ module Scribo
         contents.each do |content|
           content_path = content_path_for_zip(content)
           next unless content_path
-          zio.put_next_entry(base_path + content_path )
+          zio.put_next_entry(base_path + content_path)
           zio.write content.data
 
-          dirname  = File.dirname(base_path + content_path )
-          basename = File.basename(base_path + content_path )
+          dirname  = File.dirname(base_path + content_path)
+          basename = File.basename(base_path + content_path)
           zio.put_next_entry(dirname + '/._' + basename + '.json')
           zio.write content_meta_information(content)
         end
@@ -71,7 +102,8 @@ module Scribo
     end
 
     def content_meta_information(content)
-      JSON.pretty_generate({ content_type: content.content_type,
+      JSON.pretty_generate({ kind:         content.kind,
+                             content_type: content.content_type,
                              title:        content.title,
                              description:  content.description,
                              filter:       content.filter,
