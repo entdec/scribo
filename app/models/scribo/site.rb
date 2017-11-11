152aed8adbbf2c5d05b7fb1d4d6fced20143582f
Tom de Grunt <tom@degrunt.nl>
2017-11-11 19:36:49 +0100

    Switch to a single -visible- scribo_site.json file in the zip

--- app/models/scribo/site.rb
+++ app/models/scribo/site.rb
@@ -12,32 +12,47 @@ module Scribo
       where(name: name)
     end
 
+    def self.content_path_for_zip(path, identifier, name)
+      if path.present?
+        zip_path = path[0] == '/' ? path[1..-1] : path
+        zip_path = 'index' if zip_path.blank?
+        zip_path += '.html' if File.extname(zip_path).blank?
+        zip_path
+      elsif identifier
+        zip_path = '_identified/' + identifier.tr('/', '_')
+        zip_path += '.html' if File.extname(zip_path).blank?
+        zip_path
+      elsif name
+        zip_path = '_named/' + name
+        zip_path += '.html' if File.extname(zip_path).blank?
+        zip_path
+      end
+    end
+
     def self.import
-      Zip::File.open('site_untitled.zip') do |zip_file|
+      Zip::File.open('site_main.zip') do |zip_file|
         # Find specific entry
-        meta_info_entry = zip_file.glob('site_*/._site.json').first
+        meta_info_entry = zip_file.glob('site_*/scribo_site.json').first
         break unless meta_info_entry
 
         meta_info_site = JSON.parse(meta_info_entry.get_input_stream.read)
         # TODO: Check version numbers
         site = Site.where(scribable_type: meta_info_site['scribable_type'], scribable_id: meta_info_site['scribable_id'])
-                   .where(name: meta_info_site['name']).first
+                 .where(name: meta_info_site['name']).first
         site ||= Site.create(scribable_type: meta_info_site['scribable_type'], scribable_id: meta_info_site['scribable_id'], name: meta_info_site['name'])
 
-        zip_file.glob('**/*').reject { |e| File.basename(e.name)[0..1] == '._' }.each do |entry|
-          import_path = entry.name[entry.name.index('/')..-1].chomp('.html')
+        base_path = "site_#{meta_info_site['name']}"
 
-          meta_info_name  = File.dirname(entry.name) + '/._' + File.basename(entry.name) + '.json'
-          meta_info_entry = zip_file.find_entry(meta_info_name)
-
-          meta_info = JSON.parse(meta_info_entry.get_input_stream.read)
+        meta_info_site['contents'].each do |meta_info|
+          entry_path = base_path + '/' + content_path_for_zip(meta_info['path'], meta_info['identifier'], meta_info['name'])
+          entry = zip_file.find_entry(entry_path)
 
           content = if meta_info['identifier']
                       site.contents.find_or_create_by(site: site, identifier: meta_info['identifier'])
                     elsif meta_info['name']
                       site.contents.find_or_create_by(site: site, name: meta_info['name'])
                     else
-                      site.contents.find_or_create_by(site: site, path: import_path)
+                      site.contents.find_or_create_by(site: site, path: meta_info['path'])
                     end
 
           content.data         = entry.get_input_stream.read
@@ -66,20 +81,19 @@ module Scribo
       zip_name  = 'site_' + (name || 'untitled')
       base_path = zip_name + '/'
       stringio  = Zip::OutputStream.write_buffer do |zio|
-        zio.put_next_entry(base_path + '._site.json')
-        zio.write site_meta_information
+        meta_info = site_meta_information
 
         contents.each do |content|
-          content_path = content_path_for_zip(content)
+          content_path = content_path_for_zip(content.path, content.identifier, content.name)
           next unless content_path
           zio.put_next_entry(base_path + content_path)
           zio.write content.data
 
-          dirname  = File.dirname(base_path + content_path)
-          basename = File.basename(base_path + content_path)
-          zio.put_next_entry(dirname + '/._' + basename + '.json')
-          zio.write content_meta_information(content)
+          meta_info[:contents] << content_meta_information(content)
         end
+
+        zio.put_next_entry(base_path + 'scribo_site.json')
+        zio.write JSON.pretty_generate(meta_info)
       end
 
       # TODO: Just return string and use this elsewhere
@@ -91,45 +105,34 @@ module Scribo
     private
 
     def site_meta_information
-      JSON.pretty_generate({ version:        Scribo::VERSION,
-                             name:           name,
-                             scribable_type: scribable_type,
-                             scribable_id:   scribable_id,
-                             properties:     {} }.reject { |_, v| v.nil? })
+      { version:        Scribo::VERSION,
+        name:           name,
+        scribable_type: scribable_type,
+        scribable_id:   scribable_id,
+        properties:     {},
+        contents:       [] }.reject { |_, v| v.nil? }
     end
 
     def content_meta_information(content)
-      JSON.pretty_generate({ kind:         content.kind,
-                             content_type: content.content_type,
-                             title:        content.title,
-                             description:  content.description,
-                             filter:       content.filter,
-                             caption:      content.caption,
-                             breadcrumb:   content.breadcrumb,
-                             keywords:     content.keywords,
-                             state:        content.state,
-                             layout:       content.layout&.identifier,
-                             identifier:   content.identifier,
-                             name:         content.name,
-                             properties:   content.properties,
-                             published_at: content.published_at }.reject { |_, v| v.nil? })
+      { path:         content.path,
+        kind:         content.kind,
+        content_type: content.content_type,
+        title:        content.title,
+        description:  content.description,
+        filter:       content.filter,
+        caption:      content.caption,
+        breadcrumb:   content.breadcrumb,
+        keywords:     content.keywords,
+        state:        content.state,
+        layout:       content.layout&.identifier,
+        identifier:   content.identifier,
+        name:         content.name,
+        properties:   content.properties,
+        published_at: content.published_at }.reject { |_, v| v.nil? }
     end
 
-    def content_path_for_zip(content)
-      if content.path.present?
-        zip_path = content.path[0] == '/' ? content.path[1..-1] : content.path
-        zip_path = 'index' if zip_path.blank?
-        zip_path += '.html' if File.extname(zip_path).blank?
-        zip_path
-      elsif content.identifier
-        zip_path = '_identified/' + content.identifier.tr('/', '_')
-        zip_path += '.html' if File.extname(zip_path).blank?
-        zip_path
-      elsif content.name
-        zip_path = '_named/' + content.name
-        zip_path += '.html' if File.extname(zip_path).blank?
-        zip_path
-      end
+    def content_path_for_zip(path, identifier, name)
+      self.class.content_path_for_zip(path, identifier, name)
     end
   end
 end
