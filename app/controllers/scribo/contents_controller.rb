f983e2480743bd4b1e57d0c38670c1aeb0d9c3a7
Tom de Grunt <tom@degrunt.nl>
2017-11-17 17:02:14 +0100

    Add additional context for contents-controller as well

--- app/controllers/scribo/contents_controller.rb
+++ app/controllers/scribo/contents_controller.rb
@@ -8,7 +8,6 @@ module Scribo
     protect_from_forgery except: :show
 
     def show
-      Rails.logger.debug "server_name: '#{request.headers['SERVER_NAME']}'"
       current_site = if scribo_current_site
                        scribo_current_site
                      else
@@ -19,11 +18,20 @@ module Scribo
       @content ||= current_site&.contents&.located('/404')&.first
 
       if @content
+
+        assigns = { 'request' => ActionDispatch::RequestDrop.new(request) }
+
+        instance_variables.reject { |i| i.to_s.starts_with?('@_') }.each do |i|
+          assigns[i.to_s[1..-1]] = instance_variable_get(i)
+        end
+
+        registers = { 'controller' => self }.stringify_keys
+
         if @content.kind == 'redirect'
-          redirect_options = Content.redirect_options(@content.render(request: ActionDispatch::RequestDrop.new(request)))
+          redirect_options = Content.redirect_options(@content.render(assigns, registers))
           redirect_to redirect_options.last, status: redirect_options.first
         elsif stale?(last_modified: @content.updated_at, public: true)
-          render body: @content.render(request: ActionDispatch::RequestDrop.new(request)), content_type: @content.content_type, layout: false
+          render body: @content.render(assigns, registers), content_type: @content.content_type, layout: false
         end
       else
         render body: '404 Not Found', status: 404
