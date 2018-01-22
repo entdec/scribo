af02737628646ace7db8e10ae8bae919611bb81d
Tom de Grunt <tom@degrunt.nl>
2018-01-22 14:15:02 +0100

    Fix issues with analytics tags

--- lib/scribo/liquid/google_analytics_javascript.rb
+++ lib/scribo/liquid/google_analytics_javascript.rb
@@ -9,21 +9,23 @@ class GoogleAnalyticsJavascriptTag < Liquid::Tag
   def initialize(tag_name, markup, tokens)
     super
     if markup =~ SYNTAX
-      @code = Liquid::Expression.parse(Regexp.last_match[1]).to_s
+      @code = Liquid::Expression.parse(Regexp.last_match[1])
     else
       raise SyntaxError, "Syntax Error in 'google_analytics_javascript' - Valid syntax: yield 'tracking_id'"
     end
   end
 
-  def render(_)
+  def render(context)
+    code = @code.render(context)
     return unless Rails.env == 'production'
-    Rails.logger.warn "Inserting google analytics with code: #{@code}"
+    return unless code
+    Rails.logger.warn "Inserting google analytics with code: #{code}"
     %(<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '#{@code}', 'auto');
+  ga('create', '#{code}', 'auto');
   ga('send', 'pageview');</script>)
   end
 end
