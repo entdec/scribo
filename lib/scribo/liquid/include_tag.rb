1e6b88a9f1dd0ce1782aee5ba825262d93e07d71
Tom de Grunt <tom@degrunt.nl>
2018-03-06 11:17:02 +0100

    Includes are now rendered in context

--- lib/scribo/liquid/include_tag.rb
+++ lib/scribo/liquid/include_tag.rb
@@ -26,7 +26,7 @@ class IncludeTag < Liquid::Tag
     current_content = lookup(context.registers, 'content')
 
     content = current_content.site.contents.identified(@identifier).first
-    content&.render(context, context.registers)
+    content&.render(context.environments.first, context.registers)
   end
 end
 
