d70c3ec34c42cd1a9cb83b8b7ca8d0b99fd017b3
Tom de Grunt <tom@degrunt.nl>
2018-03-09 21:43:23 +0100

    Include maintenance standards

--- app/controllers/scribo/application_controller.rb
+++ app/controllers/scribo/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require_dependency 'concerns/maintenance_standards'
+
 module Scribo
   class ApplicationController < (Scribo.base_controller || '::ApplicationController').constantize
+    include MaintenanceStandards
   end
 end
