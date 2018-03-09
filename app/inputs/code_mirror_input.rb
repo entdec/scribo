1638608d7efc3580a5f944256ca8693fb44c9ffe
Tom de Grunt <tom@degrunt.nl>
2018-03-09 22:33:33 +0100

    Duplicate code here too, yuk

---
+++ app/inputs/code_mirror_input.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Examples:
+# = d.input :intake, as: :text, input_html: {value: @retailer.returns_call_to_actions&&@retailer.returns_call_to_actions['intake'], class: 'form-control codemirror', 'data-lang' => 'text/html'}, error: @retailer.errors.messages[:returns_call_to_actions]
+# = f.input :settings, as: :text, input_html: {class: 'form-control codemirror', 'data-lang' => 'text/json', value: JSON.pretty_generate(@retailer.settings).html_safe}
+class CodeMirrorInput < SimpleForm::Inputs::TextInput
+  def input_html_classes
+    super.push('codemirror')
+  end
+
+  def input(wrapper_options = nil)
+    template.content_tag(:div, class: 'code-mirror-input') do
+      template.concat super
+      template.concat template.content_tag('span', 'cmd/ctrl-F for search and alt-g to goto line')
+    end
+  end
+end
