34c5b326407114c8d27f8afbfde265a2beed4ecf
Tom de Grunt <tom@degrunt.nl>
2018-06-29 20:27:59 +0200

    Simplify - it's sites

--- lib/scribo/active_record_helpers.rb
+++ lib/scribo/active_record_helpers.rb
@@ -5,13 +5,7 @@ module ActiveRecordHelpers
 
   class_methods do
     def scribable(options = {})
-      configuration = {
-        name:         :sites
-      }
-
-      configuration.update(options) if options.is_a?(Hash)
-
-      has_many configuration[:name], as: :scribable, class_name: 'Scribo::Site'
+      has_many :sites, as: :scribable, class_name: 'Scribo::Site'
     end
   end
 end
