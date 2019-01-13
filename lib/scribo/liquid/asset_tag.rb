e191b02d30907c4d2bddd6499b30e3a0befa2ed4
Tom de Grunt <tom@degrunt.nl>
2019-01-13 14:32:55 +0100

    Fix this finally

--- lib/scribo/liquid/asset_tag.rb
+++ lib/scribo/liquid/asset_tag.rb
@@ -2,24 +2,33 @@
 
 # Asset tag
 #
-# {% asset 'test.png' %}
+# {%asset 'test.png'%}
+# {%asset 'test.png' height="72px"%}
+#
+# Not really used but one could pass options with json
+# {%asset 'test.png' style="height: 72px;" {"style":"test"}%}
 class AssetTag < Liquid::Tag
-  SYNTAX = /(#{Liquid::QuotedFragment})/o
+  SYNTAX = /(\"|\')(?<name>[^\"\']+)(\"|\')\s?(?<attrs>(([a-z_]+)\=\"([^\"]*)\")\s?)*(?<json>{.*})?/
 
   def initialize(tag_name, markup, options)
     super
     if markup =~ SYNTAX
-      @name = Liquid::Expression.parse(Regexp.last_match[1])
+      @name = Regexp.last_match[:name]
+      @json = Regexp.last_match[:json]
+      @json = JSON.parse(@json) if @json
+      @attrs = Regexp.last_match[:attrs]
     else
-      raise SyntaxError, "Syntax Error in 'asset' - Valid syntax: asset 'name'"
+      raise SyntaxError, "Syntax Error in 'asset' - Valid syntax: asset 'name' height='70' {'style': 'test'}, you passed: #{markup}"
     end
+
   end
 
-  def render(_context)
-    content = Content.named(@name).first
+  def render(context)
+    content = Scribo::Content.named(@name).first
     case content.content_type_group
     when 'image'
-      %[<img src="#{content.path}" alt="#{content.title}" title="#{content.caption}"/>]
+      path = content.path ? content.path : context.registers['controller'].helpers.content_url(content)
+      %[<img src="#{path}" alt="#{content.title||content.name}" title="#{content.caption||content.name}" #{@attrs}"/>]
     end
   end
 end
