0cafe401f0966779ee5c7c9fdd77e4fcabab04e4
Tom de Grunt <tom@degrunt.nl>
2019-02-10 16:19:30 +0100

    Hackfix

--- db/migrate/20190127144257_add_settings_and_translations_to_bucket.rb
+++ db/migrate/20190127144257_add_settings_and_translations_to_bucket.rb
@@ -3,5 +3,6 @@ class AddSettingsAndTranslationsToBucket < ActiveRecord::Migration[5.2]
     add_column :scribo_buckets, :settings, :jsonb, null: false, default: {}
     add_column :scribo_buckets, :translations, :jsonb, null: false, default: {}
     add_column :scribo_contents, :translations, :jsonb, null: false, default: {}
+    add_column :scribo_contents, :position, :integer
   end
 end
