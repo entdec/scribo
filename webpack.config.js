85ad1125aa65bbbbce45fd2318965ae9d85f938d
Tom de Grunt <tom@degrunt.nl>
2019-08-19 13:36:34 +0200

    frontend stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './frontend/src/javascript/scribo.js',
+  entry: ['./frontend/src/javascript/scribo.js', './frontend/src/style/scribo.scss'],
   output: {
     path: __dirname + '/frontend/dist',
     filename: 'scribo.js',
@@ -11,13 +12,17 @@ module.exports = {
     libraryTarget: 'umd'
   },
   plugins: [
-    // Dont compile or process the SCSS, let the user take care of that!
-    new CopyWebpackPlugin(
-      [
-        { from: 'frontend/src/style/**/*.scss', to: 'scss', transformPath (targetPath, absolutePath) { return targetPath.replace('frontend/src/style/', ''); } },
-      ], { copyUnmodified: true }
-    ),
-    new CleanWebpackPlugin(['frontend/dist'],  {})
+  //   // Dont compile or process the SCSS, let the user take care of that!
+  //   new CopyWebpackPlugin(
+  //     [
+  //       { from: 'frontend/src/style/**/*.scss', to: 'scss', transformPath (targetPath, absolutePath) { return targetPath.replace('frontend/src/style/', ''); } },
+  //     ], { copyUnmodified: true }
+  //   ),
+    new CleanWebpackPlugin(['frontend/dist'],  {}),
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'scribo.css',
+      allChunks: true,
+    }),
   ],
   module: {
     rules: [
@@ -36,6 +41,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
       {
         test: /\.(|ttf|eot|svg|woff2?)(\?[\s\S]+)?$/,
         use: 'file-loader',
@@ -44,6 +53,6 @@ module.exports = {
   },
   resolve: {
     modules: [path.resolve('./node_modules'), path.resolve('./src')],
-    extensions: ['.json', '.js']
+    extensions: ['.json', '.js', '.scss']
   }
 };
