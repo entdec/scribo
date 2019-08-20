9c3c8b921d2641274b109b241c64709f8265e40d
Tom de Grunt <tom@degrunt.nl>
2019-08-20 14:26:01 +0200

    Frontend stuff

--- frontend/src/javascript/scribo.js
+++ frontend/src/javascript/scribo.js
@@ -9,6 +9,7 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 export class Scribo {
   static start(application) {
+    console.log("Scribo");
     this.application = application;
     const context = require.context("./controllers", true, /\.js$/);
     this.application.load(definitionsFromContext(context));
