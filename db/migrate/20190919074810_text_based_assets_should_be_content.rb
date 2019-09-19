71bb99f41ae4eb379d8c82b2d1eec8adc720df63
Tom de Grunt <tom@degrunt.nl>
2019-09-19 12:18:08 +0200

    5.2

--- db/migrate/20190919074810_text_based_assets_should_be_content.rb
+++ db/migrate/20190919074810_text_based_assets_should_be_content.rb
@@ -1,4 +1,4 @@
-class TextBasedAssetsShouldBeContent < ActiveRecord::Migration[6.0]
+class TextBasedAssetsShouldBeContent < ActiveRecord::Migration[5.2]
   def change
     Scribo::Content.where(content_type: Scribo.config.supported_mime_types[:text]+Scribo.config.supported_mime_types[:script]+Scribo.config.supported_mime_types[:style]).update(kind: 'text')
   end
