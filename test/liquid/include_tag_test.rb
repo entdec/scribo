f3922e216df8917ed1becce5a60a41b02d6e448a
Tom de Grunt <tom@degrunt.nl>
2019-10-06 22:56:25 +0200

    Fix tests

--- test/liquid/include_tag_test.rb
+++ test/liquid/include_tag_test.rb
@@ -5,9 +5,10 @@ require 'test_helper'
 class IncludeTagTest < ActiveSupport::TestCase
   test 'does not include non-published content from current site' do
     contents       = scribo_sites(:main).contents
-    include_folder = contents.create!(path: '_includes', kind: 'folder')
-    include_menu   = contents.create!(parent: include_folder, path: 'menu', kind: 'text', data: 'included content', content_type: 'text/html', properties: { published: false })
-    subject        = contents.create!(path: '/test.html', kind: 'text', data: "|{%include 'menu'%}|", content_type: 'text/html')
+    include_folder = contents.create!(path: '/_includes', full_path: '/_includes', kind: 'folder')
+    include_menu   = contents.create!(parent: include_folder, path: 'menu.html', kind: 'text', data: 'included content', properties: { published: false })
+    subject        = contents.create!(path: '/test.html', kind: 'text', data: "|{%include 'menu'%}|")
+
 
     result = Scribo::ContentRenderService.new(subject, self).call
 
@@ -17,18 +18,18 @@ class IncludeTagTest < ActiveSupport::TestCase
   test 'includes content from current site' do
     contents       = scribo_sites(:main).contents
     include_folder = contents.create!(path: '_includes', kind: 'folder')
-    include_menu   = contents.create!(parent: include_folder, path: 'menu', kind: 'text', data: 'included content', content_type: 'text/html')
-    subject        = contents.create!(path: '/test.html', kind: 'text', data: "|{%include 'menu'%}|", content_type: 'text/html')
+    include_menu   = contents.create!(parent: include_folder, path: 'menu.html', kind: 'text', data: 'included content')
+    subject        = contents.create!(path: '/test.html', kind: 'text', data: "|{%include 'menu'%}|")
 
     result = Scribo::ContentRenderService.new(subject, self).call
 
     assert_equal '|included content|', result
   end
 
-  test 'includes identiefied content from current site' do
+  test 'includes identified content from current site' do
     contents       = scribo_sites(:main).contents
-    include_menu   = contents.create!(path: '_menu', kind: 'text', data: 'included content', content_type: 'text/html')
-    subject        = contents.create!(path: '/test.html', kind: 'text', data: "|{%include 'menu'%}|", content_type: 'text/html')
+    include_menu   = contents.create!(path: '_menu.html', kind: 'text', data: 'included content')
+    subject        = contents.create!(path: '/test.html', kind: 'text', data: "|{%include 'menu'%}|")
 
     result = Scribo::ContentRenderService.new(subject, self).call
 
@@ -38,8 +39,8 @@ class IncludeTagTest < ActiveSupport::TestCase
 
   test 'does not include content from other site' do
     include_folder = scribo_sites(:second ).contents.create!(path: '_includes', kind: 'folder')
-    scribo_sites(:second).contents.create!(parent: include_folder, path: 'menu', kind: 'text', data: 'included content', content_type: 'text/html')
-    subject = scribo_sites(:main).contents.create!(path: '/test.html', kind: 'text', data: "|{%include 'menu'%}|", content_type: 'text/html')
+    scribo_sites(:second).contents.create!(parent: include_folder, path: 'menu', kind: 'text', data: 'included content')
+    subject = scribo_sites(:main).contents.create!(path: '/test.html', kind: 'text', data: "|{%include 'menu'%}|")
 
     result = Scribo::ContentRenderService.new(subject, self).call
 
@@ -48,8 +49,8 @@ class IncludeTagTest < ActiveSupport::TestCase
 
   test 'included content receives context passed from subject' do
     include_folder = scribo_sites(:main).contents.create!(path: '_includes', kind: 'folder')
-    scribo_sites(:main).contents.create!(parent: include_folder, path: 'menu', kind: 'text', data: 'hello {{dummy.dummy_attr}}', content_type: 'text/html')
-    subject = scribo_sites(:main).contents.create!(path: '/test.html', kind: 'text', data: "{{dummy.dummy_attr}}|{%include 'menu'%}|", content_type: 'text/html')
+    scribo_sites(:main).contents.create!(parent: include_folder, path: 'menu', kind: 'text', data: 'hello {{dummy.dummy_attr}}')
+    subject = scribo_sites(:main).contents.create!(path: '/test.html', kind: 'text', data: "{{dummy.dummy_attr}}|{%include 'menu'%}|")
 
     @dummy = DummyObject.new('dummy')
     result = Scribo::ContentRenderService.new(subject, self).call
@@ -59,8 +60,8 @@ class IncludeTagTest < ActiveSupport::TestCase
 
   test 'included content receives context passed from subject as well as assigns from tag' do
     include_folder = scribo_sites(:main).contents.create!(path: '_includes', kind: 'folder')
-    scribo_sites(:main).contents.create!(parent: include_folder, path: 'menu', kind: 'text', data: 'hello {{dummy.dummy_attr}} {{name}}', content_type: 'text/html')
-    subject = scribo_sites(:main).contents.create!(path: '/test.html', kind: 'text', data: "{{dummy.dummy_attr}}|{%include 'menu' name:'bob'%}|{{name}}", content_type: 'text/html')
+    scribo_sites(:main).contents.create!(parent: include_folder, path: 'menu', kind: 'text', data: 'hello {{dummy.dummy_attr}} {{name}}')
+    subject = scribo_sites(:main).contents.create!(path: '/test.html', kind: 'text', data: "{{dummy.dummy_attr}}|{%include 'menu' name:'bob'%}|{{name}}")
 
     @dummy = DummyObject.new('dummy')
     result = Scribo::ContentRenderService.new(subject, self).call
