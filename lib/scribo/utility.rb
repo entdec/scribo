624c93890459f640266ab35909f691dbd23f095d
Tom de Grunt <tom@degrunt.nl>
2019-11-01 23:53:58 +0100

    Bugfix

--- lib/scribo/utility.rb
+++ lib/scribo/utility.rb
@@ -20,7 +20,7 @@ module Scribo
     end
 
     def kind_for_content_type(content_type)
-      MIME::Types.type_for(content_type).any? { |t| t.media_type == 'text' } ? 'text' : 'asset'
+      MIME::Types[content_type].any? { |t| t.media_type == 'text' } ? 'text' : 'asset'
     end
 
     def kind_for_path(path)
