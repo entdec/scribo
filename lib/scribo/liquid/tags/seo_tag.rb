72041f3fc6f99639e05d7c3e182e9ccc73ef4436
Tom de Grunt <tom@degrunt.nl>
2019-11-01 13:33:22 +0100

    Fixes to seo tag

--- lib/scribo/liquid/tags/seo_tag.rb
+++ lib/scribo/liquid/tags/seo_tag.rb
@@ -17,14 +17,14 @@ class SeoTag < LiquorTag
 <!-- Begin Scribo SEO tag #{Scribo::VERSION} -->
 <title>#{site.properties['title']}</title>
 <meta name="generator" content="Scribo #{Scribo::VERSION}" />
-<meta property="og:title" content="#{content.site.name}" />
+<meta property="og:title" content="#{content.site.title}" />
 <meta name="author" content="#{site.properties['author']}" />
 <meta property="og:locale" content="en_US" />
 <meta name="description" content="#{site.properties['description']}" />
 <meta property="og:description" content="#{site.properties['description']}" />
 <link rel="canonical" href="#{request.protocol + request.host}" />
 <meta property="og:url" content="#{request.protocol + request.host}" />
-<meta property="og:site_name" content="#{content.site.name}" />
+<meta property="og:site_name" content="#{content.site.title}" />
 <script type="application/ld+json">
 {"url":"#{request.protocol + request.host}","headline":"#{site.properties['title']}","name":"#{site.properties['title']}","author":{"@type":"Person","name":"#{site.properties['author']}"},"description":"#{site.properties['description']}","@type":"WebSite","@context":"https://schema.org"}</script>
 <!-- End Scribo SEO tag -->
