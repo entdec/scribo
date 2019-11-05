b597f8d8107e5a9ceb1304dddd63eb65f2c4843a
Tom de Grunt <tom@degrunt.nl>
2019-11-05 23:49:00 +0100

    Wrong conversion

--- db/migrate/20191105073053_remove_site_purpose_and_change_content_data_column_type.rb
+++ db/migrate/20191105073053_remove_site_purpose_and_change_content_data_column_type.rb
@@ -1,6 +1,6 @@
 class RemoveSitePurposeAndChangeContentDataColumnType < ActiveRecord::Migration[5.2]
   def up
-    change_column :scribo_contents, :data, :text, using: "encode(data, 'escape')"
+    change_column :scribo_contents, :data, :text, using: "convert_from(data, 'UTF-8')"
     remove_column :scribo_sites, :purpose, :string, default: 'site'
   end
 
