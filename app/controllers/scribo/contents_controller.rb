f03b1ed9878756a2ae31d5fb3c812dc621c8fc80
Tom de Grunt <tom@degrunt.nl>
2019-11-05 13:04:28 +0100

    Output to console

--- app/controllers/scribo/contents_controller.rb
+++ app/controllers/scribo/contents_controller.rb
@@ -10,6 +10,9 @@ module Scribo
     def show
       render scribo: Scribo.config.site_for_uri(URI.parse(request.url)), path: request.path
     rescue StandardError => e
+      Scribo.config.logger.error '--- Content rendering errors: ' + '-' * 49
+      Scribo.config.logger.error '=> ' + e.backtrace.map(&:to_s).join("\n") + ': ' + e.message
+      Scribo.config.logger.error '-' * 80
       render body: e.message, status: 500
     end
   end
