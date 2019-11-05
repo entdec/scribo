b91ee8c0895a84b5ec496dac1f2b0d810fa74c7b
Tom de Grunt <tom@degrunt.nl>
2019-11-05 10:32:18 +0100

    5.2

--- db/migrate/20191105091503_remove_published_at_from_contents.rb
+++ db/migrate/20191105091503_remove_published_at_from_contents.rb
@@ -1,4 +1,4 @@
-class RemovePublishedAtFromContents < ActiveRecord::Migration[6.0]
+class RemovePublishedAtFromContents < ActiveRecord::Migration[5.2]
   def change
     remove_column :scribo_contents, :published_at, :datetime
   end
