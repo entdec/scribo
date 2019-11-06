0db441ed730abf91685d55aaee6bf035af8718db
Tom de Grunt <tom@degrunt.nl>
2019-11-06 11:05:11 +0100

    Oops

--- test/dummy/config/initializers/scribo.rb
+++ test/dummy/config/initializers/scribo.rb
@@ -4,6 +4,6 @@ Scribo.setup do |config|
   config.base_controller = '::ApplicationController'
   # config.logger = Logger.new('/dev/null')
   config.site_for_uri = lambda do |_uri|
-    Account.find_by(name: 'One').sites.located('/').first
+    Account.find_by(name: 'One').sites.for_path('/').first
   end
 end
