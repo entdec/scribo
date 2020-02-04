b0c9cf84964618c2dc93667bc029be454506ca8a
Tom de Grunt <tom@degrunt.nl>
2020-02-04 20:42:57 +0100

    fix

--- app/drops/scribo/action_dispatch/request_drop.rb
+++ app/drops/scribo/action_dispatch/request_drop.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
+require_relative '../application_drop'
 
 module Scribo
-  class ActionDispatch::RequestDrop < ApplicationDrop
-    delegate :fullpath, :host, :scheme, :ip, :media_type, :query_parameters, :uuid, :request_method, to: :@object
+  module ActionDispatch
+    class RequestDrop < ::Scribo::ApplicationDrop
+      delegate :fullpath, :host, :scheme, :ip, :media_type, :query_parameters, :uuid, :request_method, to: :@object
 
-    def headers
-      @object.headers.to_h
+      def headers
+        @object.headers.to_h
+      end
     end
   end
 end
