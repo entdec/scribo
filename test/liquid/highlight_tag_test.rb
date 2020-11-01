78d419dab812e8e8261025fda35347f253e1e0ab
Tom de Grunt <tom@degrunt.nl>
2020-11-01 16:12:03 +0100

    Some more tests

--- test/liquid/highlight_tag_test.rb
+++ test/liquid/highlight_tag_test.rb
@@ -11,4 +11,26 @@ class HighlightTagTest < ActiveSupport::TestCase
 
     assert_equal "<figure class=\"highlight\"><pre><code class=\"language-ruby\" data-lang=\"ruby\"><span class=\"k\">def</span> <span class=\"nf\">foo</span>\n<span class=\"nb\">puts</span> <span class=\"s1\">'foo'</span>\n<span class=\"k\">end</span></code></pre></figure>", result
   end
+
+  test 'does do highlighting too' do
+    contents       = scribo_sites(:main).contents
+
+    data = <<~CONTENT
+      {% highlight ruby %}
+      def print_hi(name)
+        puts "Hi, #{name}"
+      end
+      print_hi('Tom')
+      #=> prints 'Hi, Tom' to STDOUT.
+      {% endhighlight %}
+    CONTENT
+
+    subject        = contents.create!(path: '/test.html', kind: 'text', data: data)
+
+    result = Scribo::ContentRenderService.new(subject, self).call
+
+    assert_equal "<figure class=\"highlight\"><pre><code class=\"language-ruby\" data-lang=\"ruby\"><span class=\"k\">def</span> <span class=\"nf\">print_hi</span><span class=\"p\">(</span><span class=\"nb\">name</span><span class=\"p\">)</span>\n  <span class=\"nb\">puts</span> <span class=\"s2\">\"Hi, test_does_do_highlighting_too\"</span>\n<span class=\"k\">end</span>\n<span class=\"n\">print_hi</span><span class=\"p\">(</span><span class=\"s1\">'Tom'</span><span class=\"p\">)</span>\n<span class=\"c1\">#=&gt; prints 'Hi, Tom' to STDOUT.</span></code></pre></figure>\n", result
+  end
+
+
 end
