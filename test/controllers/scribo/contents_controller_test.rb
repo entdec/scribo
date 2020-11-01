c85374dfb65f56871b935fbe253e568378c639c8
Tom de Grunt <tom@degrunt.nl>
2020-11-01 11:36:45 +0100

    Fix test

--- test/controllers/scribo/contents_controller_test.rb
+++ test/controllers/scribo/contents_controller_test.rb
@@ -6,6 +6,13 @@ module Scribo
   class ContentsControllerTest < ::ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
+    setup do
+      Account.first.current!
+      @site = Scribo::Site.create!(scribable: Account.current)
+      assert_equal Account.current, @site.scribable
+      @site.contents.create!(kind: 'text', path: 'index.html', data: 'Hello')
+    end
+
     test 'should get show' do
       get root_url
       assert_response :success
