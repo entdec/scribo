cd844be6681058f706b81a31f1ad70b5c4fea1cd
Tom de Grunt <tom@degrunt.nl>
2020-11-12 21:07:28 +0100

    Steps

--- cli/scribo.cr
+++ cli/scribo.cr
@@ -1,34 +1,64 @@
 require "compress/zip"
 require "admiral"
+require "yaml"
+require "http/client"
 
-class Scribo < Admiral::Command
-  define_help description: "Scribo command line utility"
+class Scribo
+  def initialize
+  end
 
-  class Publish < Admiral::Command
-    def run
-      file_name = "#{File.basename(Dir.current)}.zip"
-      if !File.exists?("_config.yml")
-        puts "No _config.yml found, are you in the correct folder?"
-        exit
-      end
+  def zip_and_upload
+    zip_file_name = "#{File.basename(Dir.current)}.zip"
+    tempfile = File.tempfile(zip_file_name)
+    File.open(tempfile.path, "w") do |file|
+      Compress::Zip::Writer.open(file) do |zip|
 
-      File.open(file_name, "w") do |file|
-        Compress::Zip::Writer.open(file) do |zip|
-
-          Dir.glob("**/*").each do |file_name|
-            next if File.directory?(file_name)
-            zip.add file_name, File.open(file_name)
-          end
+        Dir.glob("**/*").each do |file_name|
+          next if File.directory?(file_name)
+          zip.add file_name, File.open(file_name)
         end
       end
     end
+
+    io = IO::Memory.new
+    builder = HTTP::FormData::Builder.new(io, "aA47")
+    builder.file("files[]", File.open(tempfile.path), HTTP::FormData::FileMetadata.new(filename: zip_file_name))
+    builder.finish
+
+    response = HTTP::Client.post(import_url, headers: HTTP::Headers{"User-Agent" => "Scribo"}, body: io.to_s)
+
+    tempfile.delete
   end
 
-  register_sub_command publish : Publish, description: "Publishes the current site"
+  def config
+    config = YAML.parse(File.read("_config.yml"))
+    config = config["cli"]
+  end
 
-  def run
-    puts help
+  def import_url
+    "#{config["endpoint"]}/import"
   end
 end
 
-Scribo.run
+class ScriboCommand < Admiral::Command
+  define_help description: "Scribo command line utility"
+
+  class PublishCommand < Admiral::Command
+    def run
+      Scribo.new.zip_and_upload
+    end
+  end
+
+  register_sub_command publish : PublishCommand, description: "Publishes the current site"
+
+  def run
+    puts help
+
+    if !File.exists?("_config.yml")
+      puts "\nNo _config.yml found, are you in the correct folder?"
+      exit
+    end
+  end
+end
+
+ScriboCommand.run
