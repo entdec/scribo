4c543522637ffb9859c3ac92ee7cdd764e407969
Tom de Grunt <tom@degrunt.nl>
2020-11-14 22:27:07 +0100

    Add all attributes

--- app/drops/scribo/paginator_drop.rb
+++ app/drops/scribo/paginator_drop.rb
@@ -3,6 +3,7 @@
 require_dependency 'scribo/application_drop'
 
 module Scribo
+  # https://jekyllrb.com/docs/pagination/#liquid-attributes-available
   class PaginatorDrop < ApplicationDrop
     def initialize(site, content)
       @site = site
@@ -11,6 +12,38 @@ module Scribo
 
     def posts
       @site.contents.posts.to_a
+    end
+
+    def page
+      1
+    end
+
+    def per_page
+      1
+    end
+
+    def total_posts
+      posts.size
+    end
+
+    def total_pages
+      1
+    end
+
+    def previous_page
+      nil
+    end
+
+    def previous_page_path
+      nil
+    end
+
+    def next_page
+      nil
+    end
+
+    def next_page_path
+      nil
     end
   end
 end
