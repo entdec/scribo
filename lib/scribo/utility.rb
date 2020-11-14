1e511668be0ccdc07346aac9139fcf2b2ed86362
Tom de Grunt <tom@degrunt.nl>
2020-11-14 21:17:40 +0100

    Use of constants

--- lib/scribo/utility.rb
+++ lib/scribo/utility.rb
@@ -4,6 +4,40 @@ module Scribo
   module Utility
     module_function
 
+    ADDITIONAL_EXTENSIONS = {
+      'text/css' => %w[scss sass],
+      'text/html' => %w[md markdown mkd slim],
+      'application/javascript' => %w[es6 babel jsx js]
+    }.freeze
+
+    KNOWN_TEXT_FILES = %w[Gemfile].freeze
+    KNOWN_TEXT_EXTENSIONS = %w[scss sass less slim es6 babel jsx json].freeze
+
+    FILTER_FOR_EXTENSION = {
+      'scss' => 'scss',
+      'sass' => 'sass',
+      'md' => 'markdown',
+      'markdown' => 'markdown',
+      'mkd' => 'markdown',
+      'slim' => 'slim',
+      'es6' => 'babel',
+      'babel' => 'babel',
+      'jsx' => 'babel'
+    }.freeze
+
+    OUTPUT_CONTENT_TYPE_FOR_EXTENSION = {
+      'scss' => 'text/css',
+      'sass' => 'text/css',
+      'md' => 'text/html',
+      'markdown' => 'text/html',
+      'mkd' => 'text/html',
+      'slim' => 'text/html',
+      'es6' => 'application/javascript',
+      'babel' => 'application/javascript',
+      'jsx' => 'application/javascript',
+      'js' => 'application/javascript'
+    }.freeze
+
     def yaml_safe_parse(text)
       permitted_classes = [Date, Time]
       YAML.safe_load(text, permitted_classes)
@@ -24,9 +58,8 @@ module Scribo
     end
 
     def kind_for_path(path)
-      known_text_files = %[Gemfile]
-      known_text = %w[scss sass less slim es6 babel jsx json]
-      if known_text.include?(File.extname(path)[1..-1].to_s) || known_text_files.include?(File.basename(path))
+      if KNOWN_TEXT_EXTENSIONS.include?(File.extname(path)[1..-1].to_s) ||
+         KNOWN_TEXT_FILES.include?(File.basename(path))
         'text'
       else
         MIME::Types.type_for(path).any? { |t| t.media_type == 'text' } ? 'text' : 'asset'
@@ -34,18 +67,7 @@ module Scribo
     end
 
     def filter_for_path(path)
-      case File.extname(path.to_s)[1..-1].to_s
-      when 'scss'
-        'scss'
-      when 'sass'
-        'sass'
-      when 'md', 'markdown', 'mkd'
-        'markdown'
-      when 'slim'
-        'slim'
-      when 'es6', 'babel', 'jsx'
-        'babel'
-      end
+      FILTER_FOR_EXTENSION[File.extname(path.to_s)[1..-1].to_s]
     end
 
     def variations_for_path(path)
@@ -53,15 +75,7 @@ module Scribo
       result = []
       MIME::Types.type_for(path).each do |mime_type|
         result += mime_type.extensions if mime_type.extensions
-        additional_extensions = case mime_type.content_type
-                                when 'text/css'
-                                  %w[scss sass]
-                                when 'text/html'
-                                  %w[md markdown mkd slim]
-                                when 'application/javascript'
-                                  %w[es6 babel jsx js]
-                                end
-        result += additional_extensions if additional_extensions.present?
+        result += ADDITIONAL_EXTENSIONS[mime_type.content_type] || []
       end
       result += [File.extname(path).gsub(/^\./, '')]
       dir = File.dirname(path)
@@ -74,18 +88,7 @@ module Scribo
     end
 
     def output_content_type(content)
-      case File.extname(content.path)[1..-1].to_s
-      when 'scss', 'sass'
-        'text/css'
-      when 'md', 'markdown', 'mkd'
-        'text/html'
-      when 'slim'
-        'text/html'
-      when 'es6', 'babel', 'jsx', 'js'
-        'application/javascript'
-      else
-        content.content_type
-      end
+      OUTPUT_CONTENT_TYPE_FOR_EXTENSION[File.extname(content.path)[1..-1].to_s] || content.content_type
     end
   end
 end
