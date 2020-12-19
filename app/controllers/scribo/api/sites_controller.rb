3ab562c339ee7c8b9c966d12d1655f5df533602d
Tom de Grunt <tom@degrunt.nl>
2020-12-19 22:09:02 +0100

    Debug stuff

--- app/controllers/scribo/api/sites_controller.rb
+++ app/controllers/scribo/api/sites_controller.rb
@@ -7,9 +7,11 @@ module Scribo
       skip_before_action :verify_authenticity_token
       def import
         sgid = request.authorization&.split&.last
+        Rails.logger.info "sgid: #{sgid}"
         head(400) && return unless sgid
 
         scribable = GlobalID::Locator.locate_signed(sgid, for: 'scribo')
+        Rails.logger.info "scribable: #{scribable}"
 
         head(401) && return unless scribable
 
