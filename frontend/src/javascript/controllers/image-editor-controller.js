8525de95532a86e2819d45771ee6009ee220eaf7
Tom de Grunt <tom@degrunt.nl>
2020-12-29 20:29:53 +0100

    Stuff

--- frontend/src/javascript/controllers/image-editor-controller.js
+++ frontend/src/javascript/controllers/image-editor-controller.js
@@ -4,6 +4,7 @@ import Painterro from 'painterro'
 export default class extends Controller {
   static targets = ["textarea"];
   connect() {
+    console.log('connected - image-editor');
     const self = this;
     Painterro().show(this.data.get('url'))
   }
