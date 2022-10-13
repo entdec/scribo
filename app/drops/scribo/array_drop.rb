d5ac17261ecaabf6554a70cb7a4e4bbd484349fb
Tom de Grunt <tom@degrunt.nl>
2022-10-13 13:57:43 +0200

    Support some array methods

--- app/drops/scribo/array_drop.rb
+++ app/drops/scribo/array_drop.rb
@@ -4,6 +4,8 @@ require_dependency 'scribo/application_drop'
 
 module Scribo
   class ArrayDrop < ApplicationDrop
+    delegate :each, :map, :size, :length, :first, :last, :empty?, :join, to: :@object
+
     def ==(other)
       @object.include?(other) || @object == other
     end
