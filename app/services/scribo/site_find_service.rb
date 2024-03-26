c631c782e906ecdbad81384de0974e518508313b
Syed Waqas Ali Shah <s.waqas@rapidlogistics.nl>
2024-03-26 10:29:03 +0400

    fix search site scope for all accounts

--- app/services/scribo/site_find_service.rb
+++ app/services/scribo/site_find_service.rb
@@ -17,7 +17,8 @@ module Scribo
       scribable = Scribo.config.scribable_for_request(options[:request])
       return nil unless scribable
 
-      scope = scribable.sites
+      # scope = scribable.sites
+      scope = Scribo::Site.all
       scope = scope.for_host(options[:host])
       scope = scope.for_path(options[:path]) if options[:path]
       scope.first
